2eee8ce8897f6ba8fba688414c9aaa4ea6972b71
oclavo <141341283+oclavo@users.noreply.github.com>
2025-06-27 08:36:00 -0300

    u

--- main.js
+++ main.js
@@ -61,18 +61,18 @@ function geraSenha() {
     }
     campoSenha.value = senha;
     classificaSenha();
+
+    }
+    function classificaSenha(){
 forcaSenha.classList.remove('fraca','media','forte');
 
-}
-function classificaSenha(){
-forcaSenha.classList.remove('fraca','media','forte');
-if (tamanhoSenha > 11){
+    if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
 }
-else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
+    else if (tamanhoSenha > 5 && tamanhoSenha < 12 ){
 forcaSenha.classList.add('media');
 }
-else if (tamanhoSenha <= 5){
+    else if (tamanhoSenha <= 5){
 forcaSenha.classList.add('fraca');
 }
 }
