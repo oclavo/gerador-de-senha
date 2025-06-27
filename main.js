b757e26ec6e93a71d069b2e21d23a0e569c3957f
oclavo <141341283+oclavo@users.noreply.github.com>
2025-06-27 08:21:28 -0300

    k

--- main.js
+++ main.js
@@ -65,7 +65,8 @@ forcaSenha.classList.remove('fraca','media','forte');
 if (tamanhoSenha > 11){
 forcaSenha.classList.add('forte');
 }
-else {
+else if (tamanhoSenha > 5 && tamanhoSenha < 12 )
+ {
 forcaSenha.classList.add('media');
 }
 }
